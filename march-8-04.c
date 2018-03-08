64075eb08c7ace68168bebcba8ba21daa1b80a2f
Reazul Islam <reazulislam137@gmail.com>
2018-03-08 22:07:48 +0600

    first commit

---
+++ march-8-04.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, product;
+
+    printf("input the first integer  : ");
+    scanf("%d",&a);
+
+    printf("input the second integer : ");
+    scanf("%d",&b);
+
+    product = a * b;
+    printf("product of the above two integers : %d",product);
+    return 0;
+}
